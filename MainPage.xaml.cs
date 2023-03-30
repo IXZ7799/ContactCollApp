e1fc8aaf47d0a2f51ebc6889d55b8c202968d871
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-30 03:23:20 +0100

    New btn

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,5 +20,9 @@ public partial class MainPage : ContentPage
         Navigation.PushModalAsync(new CustomerDetails(selectedCust, true), true);
     }
 
+    private void btnAdd_Clicked()
+    {
+
+    }
 }
 
