b97f7249d2672015981e9bf872acde640bc79262
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-30 01:28:25 +0100

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,17 @@
-﻿namespace ContactCollApp;
+﻿using System.Collections.ObjectModel;
+
+namespace ContactCollApp;
 
 public partial class MainPage : ContentPage
 {
+    public CustomerCollection currentCustomers;
 
     public MainPage()
     {
         InitializeComponent();
+
+        currentCustomers = CustomerCollection.MakeTestCustomers();
+        collectionView.ItemsSource = currentCustomers.CustomerList;
     }
 
 }
