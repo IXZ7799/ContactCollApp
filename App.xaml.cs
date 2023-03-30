9488f789a4761f11dde44b9a3087a7c56a88166c
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-30 15:18:40 +0100

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,10 +3,23 @@
 public partial class App : Application
 {
     public Customer selectedCustomer;
+    private static CustomerSQLiteDatabase customerDB;
     public App()
     {
         InitializeComponent();
 
         MainPage = new AppShell();
+    }
+
+    public static CustomerSQLiteDatabase CustomerDatabase
+    {
+        get
+        {
+            if (customerDB == null)
+            {
+                customerDB = new CustomerSQLiteDatabase();
+            }
+            return customerDB;
+        }
     }
 }
