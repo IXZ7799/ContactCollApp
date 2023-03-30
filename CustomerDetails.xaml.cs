07cd8013ffcac03cd694a3397a537c7d8b46a792
IXZ7799 <78650654+IXZ7799@users.noreply.github.com>
2023-03-30 03:43:09 +0100

    Update CustomerDetails.xaml.cs

--- CustomerDetails.xaml.cs
+++ CustomerDetails.xaml.cs
@@ -8,13 +8,48 @@ public partial class CustomerDetails : ContentPage
         InitializeComponent();
         this.isUpdated = isUpdated;
 
+        this.pckImage.Items.Clear();
+        this.pckImage.ItemsSource = CustomerCollection.imageNames;
+
         App thisApp = Application.Current as App;
         thisApp.selectedCustomer = cust;
         this.gridCustomer.BindingContext = thisApp.selectedCustomer;
+
+        if (cust != null)
+            this.pckImage.SelectedItem = cust.ImageName;
     }
 
-    private void pckImage_SelectedIndexChanged()
+    private void pckImage_SelectedIndexChanged(object sender, EventArgs e)
     {
+        this.imgPic.Source = this.pckImage.SelectedItem as string;
+    }
 
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+        string selectedImage = CustomerCollection.DEFAULT_IMAGE;
+        string name = this.txtName.Text;
+        string address = this.txtAddress.Text;
+
+        if (this.pckImage.SelectedItem != null)
+            selectedImage = this.pckImage.SelectedItem as string;
+
+        if (isUpdated)
+        {
+            App thisApp = Application.Current as App;
+
+            thisApp.selectedCustomer.Name = name;
+            thisApp.selectedCustomer.Address = address;
+            thisApp.selectedCustomer.ImageName = selectedImage;
+
+            int pos = MainPage.currentCustomers.CustomerList.IndexOf(thisApp.selectedCustomer);
+            MainPage.currentCustomers.CustomerList.RemoveAt(pos);
+
+            MainPage.currentCustomers.CustomerList.Insert(pos, thisApp.selectedCustomer);
+        }
+        else
+        {
+            Customer newCust = new Customer(name, address, CustomerCollection.IdCustomer++, selectedImage);
+            MainPage.currentCustomers.CustomerList.Add(newCust);
+        }
     }
 }
